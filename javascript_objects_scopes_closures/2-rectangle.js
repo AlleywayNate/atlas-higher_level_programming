2ab3818edb90fef9f53514e5cdfe9bb22a8b4e6d
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 15:50:33 -0500

    Forgot semistandard

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        // This block is empty
-        // If width or height are invalid, create an empty Rectangle
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // This block is empty
+      // If width or height are invalid, create an empty Rectangle
+    } else {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
