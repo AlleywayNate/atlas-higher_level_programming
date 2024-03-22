52dc104ce489cd8ae22c8503e10acb3409ee075d
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 15:37:38 -0500

    code changes

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-    } else {
-      this.width = w;
-      this.height = h;
+    constructor (w, h) {
+      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+        // If width or height are invalid, create an empty Rectangle
+        console.log('Rectangle {}');
+      } else {
+        this.width = w;
+        this.height = h;
+      }
     }
   }
-}
-
-module.exports = Rectangle;
+  
+  module.exports = Rectangle;
+  
