4e1e485517145da8c1c1922d39d4f866a637519d
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 13:49:59 -0500

    semistandard --fix 0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-    // Constructor and methods can be added here if needed
+  // Constructor and methods can be added here if needed
 }
 
 module.exports = Rectangle;
