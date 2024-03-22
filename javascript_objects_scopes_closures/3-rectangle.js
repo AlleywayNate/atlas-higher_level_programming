ba27fa12ae0fb36555f06fb261ba005284dcb29c
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 16:28:23 -0500

    fixed class

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
-
-function Rectangle (w, h) {
-  this.width = w;
-  this.height = h;
-}
-
-Rectangle.prototype.print = function () {
-  for (let i = 0; i < this.height; i++) {
-    let line = '';
-    for (let j = 0; j < this.width; j++) {
-      line += 'X';
-    }
-    console.log(line);
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
-};
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}
 
 module.exports = Rectangle;
