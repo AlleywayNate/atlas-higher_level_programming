0c3b59b660a8d36897cea897759758dc4cb4ee6e
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 15:40:14 -0500

    didnt need and else statment

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,7 @@
 class Rectangle {
     constructor (w, h) {
       if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        // If width or height are invalid, create an empty Rectangle
-        console.log('Rectangle {}');
-      } else {
-        this.width = w;
-        this.height = h;
       }
     }
-  }
-  
+}
   module.exports = Rectangle;
-  
