df3fa19035f567e186fd8238dcf928c20197bd41
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-22 15:45:36 -0500

    code changes with added else fixing semi validation

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,13 @@
 class Rectangle {
     constructor (w, h) {
       if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+        // This block is empty
+        // If width or height are invalid, create an empty Rectangle
+      } else {
+        this.width = w;
+        this.height = h;
       }
     }
-}
+  }
+  
   module.exports = Rectangle;
