49e487aa4e049e1a6250f28a419358afce720f33
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-03-24 19:18:09 -0500

    changed code to test out stuff and learn more

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+module.exports = {
+  nbOccurences: function (list, searchElement) {
+    let count = 0;
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === searchElement) {
+        count++;
+      }
+    }
+    return count;
+  }
 };
