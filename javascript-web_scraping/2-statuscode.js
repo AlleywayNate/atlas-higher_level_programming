d316b64c5fdf3eb56093d8222ae729ea9cbc4c45
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 08:37:36 -0500

    updating code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+
+// Extracting the URL to request from the command line arguments
+const url = process.argv[2];
+
+// Making an HTTP GET request to the specified URL
+request(url, function(error, response) {
+  // If there's no error and a response is received
+  if (!error && response) {
+    // Displaying the status code of the response
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    // If an error occurs or no response is received, display an error message
+    console.error(error || 'No response received');
+  }
 });
