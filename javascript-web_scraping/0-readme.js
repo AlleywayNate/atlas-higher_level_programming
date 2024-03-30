b509fe0c6de5354c8c9cb310b3811cec49dfaa36
AlleywayNate <nathenwilliams600@gmail.com>
2024-03-30 05:15:02 -0500

    semistandered fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,13 +5,12 @@
 // Importing the 'fs' module (file system) from the Node.js standard library.
 const fs = require('fs');
 
-// Asynchronously reads the content of a file. 
+// Asynchronously reads the content of a file.
 // process.argv is an array containing the command line arguments passed when the Node.js process was launched.
 // process.argv[2] contains the path of the file to be read, which is passed as an argument when running the script.
 // 'utf8' specifies the encoding of the file (UTF-8).
 // The third parameter is a callback function to be executed once the file is read.
-fs.readFile(process.argv[2], 'utf8', function(error, content) {
-
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   // If an error occurred during file reading or processing,
   // it will be captured here. If there's no error, 'error' will be null.
   if (error) {
