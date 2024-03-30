854a699ad4f939694aa6e2a03e4c2c3e5331260a
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 08:25:21 -0500

    Fixing 569 files to fix issues and errors

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,11 +2,17 @@
 // Importing the 'request' module.
 const request = require('request');
 
-// Making an HTTP GET request to the URL provided as a command line argument (process.argv[2]).
-// This code fetches the resource specified by the URL.
-// The 'response' event is emitted when a response is received.
-// The callback function provided to 'on' handles the response.
-request.get(process.argv[2]).on('response', function (response) {
-  // Logging the status code of the response to the console.
-  console.log(`code: ${response.statusCode}`);
+// Extracting the URL to request from the command line arguments
+const url = process.argv[2];
+
+// Making an HTTP GET request to the specified URL
+request(url, function(error, response) {
+  // If there's no error and a response is received
+  if (!error && response) {
+    // Displaying the status code of the response
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    // If an error occurs or no response is received, display an error message
+    console.error(error || 'No response received');
+  }
 });
