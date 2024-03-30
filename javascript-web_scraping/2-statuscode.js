15f2d072d5f57ab2bbc357c1aff65b667b9511ae
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 08:34:55 -0500

    reverted code to see if it works

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-// Importing the 'request' module.
 const request = require('request');
-
-// Extracting the URL to request from the command line arguments
-const url = process.argv[2];
-
-// Making an HTTP GET request to the specified URL
-request(url, function(error, response) {
-  // If there's no error and a response is received
-  if (!error && response) {
-    // Displaying the status code of the response
-    console.log(`code: ${response.statusCode}`);
-  } else {
-    // If an error occurs or no response is received, display an error message
-    console.error(error || 'No response received');
-  }
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
