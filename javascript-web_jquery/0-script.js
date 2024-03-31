d25d8a39fd2df3fa562db98f6289638d410ffcce
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 20:25:51 -0500

    change files later

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+// Event listener to execute the script when the DOM content is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecting the <header> element using document.querySelector()
+    const header = document.querySelector('header');
+  
+    // Changing the text color of the <header> element to red
+    header.style.color = '#FF0000';
+  });
+  
