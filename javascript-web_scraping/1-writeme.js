2cac0a1233d58be9843d5374008fddb40d18f951
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 20:15:27 -0500

    fixing errors

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,22 +2,18 @@
 
 const fs = require('fs');
 
-// Check if the correct number of arguments is provided
+// Check if the correct number of arguments are provided
 if (process.argv.length !== 4) {
-  console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
-const filePath = process.argv[2]; // File path provided as the first argument
-const stringToWrite = process.argv[3]; // String to write provided as the second argument
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
 
 // Write the string to the file
-fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
+fs.writeFile(filePath, stringToWrite, 'utf8', (error) => {
   if (error) {
-    // If an error occurred during writing, print the error object
     console.error(error);
-  } else {
-    // If writing was successful, print a success message
-    console.log(`Content has been written to ${filePath}`);
   }
 });
