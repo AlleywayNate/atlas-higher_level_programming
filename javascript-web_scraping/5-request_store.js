b69c6d55a50c86a35625c37961e9484f1e34cb53
Nathen Williams <nathenwilliams600@gmail.com>
2024-03-30 20:42:24 -0500

    changed to code to work and become more understandable

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,27 +1,33 @@
 #!/usr/bin/node
-// Importing the 'request' module.
+// Importing the fs (file system) module to work with files
+const fs = require('fs');
+
+// Importing the request module to make HTTP requests
 const request = require('request');
 
-// Making a request to the URL provided as a command-line argument (process.argv[2]).
-// The callback function handles the response.
-request(process.argv[2], function (error, response, body) {
-  // If there's no error during the request, enter the block.
-  if (!error) {
-    // Parse the response body (which is in JSON format) to access its properties.
-    const results = JSON.parse(body).results;
+// Retrieving the URL from the command line arguments
+const url = process.argv[2];
 
-    // The 'results' array contains information about Star Wars movies.
-    // We want to count the number of movies in which the character with ID 18 (i.e., Anakin Skywalker) appears.
-    // We use the reduce() function to iterate over each movie in the 'results' array and accumulate the count.
-    console.log(results.reduce((count, movie) => {
-      // For each movie, we check if any character's ID ends with '/18/', which indicates the character is Anakin Skywalker.
-      // If such a character is found, we increment the count by 1.
-      // We use the find() method to search for the character with the specified ID.
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
-    // The count is initially set to 0. If Anakin Skywalker is found in any movie, the count increases by 1.
-    // Finally, the total count of movies featuring Anakin Skywalker is logged to the console.
+// Retrieving the file path from the command line arguments
+const filePath = process.argv[3];
+
+// Making an HTTP GET request to the specified URL
+request(url, function(error, response, body) {
+  // Checking if an error occurred during the request
+  if (error) {
+    // If an error occurred, printing the error to the console
+    console.error(error);
+  } else {
+    // Writing the body response to the specified file path
+    fs.writeFile(filePath, body, 'utf-8', function(err) {
+      // Checking if an error occurred while writing to the file
+      if (err) {
+        // If an error occurred, printing the error to the console
+        console.error(err);
+      } else {
+        // If no error occurred, printing a success message
+        console.log(`The contents of the webpage have been stored in ${filePath}`);
+      }
+    });
   }
 });
